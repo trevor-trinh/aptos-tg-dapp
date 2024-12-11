210d30f052ea1e91a6475f4bacd102d2353498a8
Trevor Trinh <50549133+trevor-trinh@users.noreply.github.com>
2024-12-10 17:02:21 -0800

    init

---
+++ frontend/components/WalletProvider.tsx
@@ -0,0 +1,36 @@
+import { PropsWithChildren } from "react";
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+// Internal components
+import { useToast } from "@/components/ui/use-toast";
+// Internal constants
+import { NETWORK, APTOS_API_KEY } from "@/constants";
+
+export function WalletProvider({ children }: PropsWithChildren) {
+  const { toast } = useToast();
+
+  return (
+    <AptosWalletAdapterProvider
+      autoConnect={true}
+      dappConfig={{
+        network: NETWORK,
+        mizuwallet: {
+          // Learn more https://docs.mizu.io/docs/preparation/mizu-app-id
+          appId: undefined,
+          // Learn more https://docs.mizu.io/docs/preparation/manifest-json
+          manifestURL: "https://assets.mz.xyz/static/config/mizuwallet-connect-manifest.json",
+          aptosApiKey: APTOS_API_KEY,
+        },
+      }}
+      optInWallets={["Mizu Wallet"]}
+      onError={(error) => {
+        toast({
+          variant: "destructive",
+          title: "Error",
+          description: error || "Unknown wallet error",
+        });
+      }}
+    >
+      {children}
+    </AptosWalletAdapterProvider>
+  );
+}
